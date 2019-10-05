173bddceec654155b6c7a4ee1699b27f33608d1b
PaKas <KasymbekovPN@yandex.ru>
2019-10-05 17:27:54 +0300

    start branch

---
+++ hw10-hibernate/src/main/java/ru/otus/kasymbekovPN/HW10/Main.java
@@ -0,0 +1,43 @@
+package ru.otus.kasymbekovPN.HW10;
+
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.kasymbekovPN.HW10.api.dao.UserDao_;
+import ru.otus.kasymbekovPN.HW10.api.model.User;
+import ru.otus.kasymbekovPN.HW10.api.service.DBServiceUser_;
+import ru.otus.kasymbekovPN.HW10.api.service.DbServiceUserImpl_;
+import ru.otus.kasymbekovPN.HW10.hibernate.HibernateUtils_;
+import ru.otus.kasymbekovPN.HW10.hibernate.dao.UserDaoHibernate_;
+import ru.otus.kasymbekovPN.HW10.hibernate.sessionManager.SessionManagerHibernate_;
+
+import java.util.Optional;
+
+public class Main {
+
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        // Все главное см в тестах
+        SessionFactory sessionFactory = HibernateUtils_.buildSessionFactory("hibernate-test.cfg.xml", User.class);
+
+        SessionManagerHibernate_ sessionManager = new SessionManagerHibernate_(sessionFactory);
+        UserDao_ userDao = new UserDaoHibernate_(sessionManager);
+        DBServiceUser_ dbServiceUser = new DbServiceUserImpl_(userDao);
+
+        long id = dbServiceUser.saveUser(new User(0, "Вася"));
+        Optional<User> mayBeCreatedUser = dbServiceUser.getUser(id);
+
+        id = dbServiceUser.saveUser(new User(1L, "А! Нет. Это же совсем не Вася"));
+        Optional<User> mayBeUpdatedUser = dbServiceUser.getUser(id);
+
+        outputUserOptional("Created user", mayBeCreatedUser);
+        outputUserOptional("Updated user", mayBeUpdatedUser);
+    }
+
+    private static void outputUserOptional(String header, Optional<User> mayBeUser) {
+        System.out.println("-----------------------------------------------------------");
+        System.out.println(header);
+        mayBeUser.ifPresentOrElse(System.out::println, () -> logger.info("User not found"));
+    }
+}
